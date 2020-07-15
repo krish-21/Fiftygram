74d4b1019f4f50744a63118ace980ee1eee9ed84
Krishna Nanduri <krishna.nanduri@icloud.com>
2020-07-15 21:10:18 +0530

    added custom filter - pixellate

--- Fiftygram/ContentView.swift
+++ Fiftygram/ContentView.swift
@@ -8,9 +8,176 @@
 
 import SwiftUI
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
+
 struct ContentView: View {
+    // the image to display
+    @State private var image: Image?
+    
+    // Boolean to show the sheet
+    @State private var showingImagePicker = false
+    
+    // UIImage passed from ImagePicker
+    //         to load Image in loadImage()
+    @State private var inputImage: UIImage?
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                // Display Image
+                image?
+                    .resizable()
+                    .scaledToFit()
+                
+                // Space everything properly
+                Spacer()
+                
+                // Sepia
+                Button(action: {
+                    self.applySepia()
+                }) {
+                    CustomButtonText(buttonText: "Sepia", buttonColor: Color.red)
+                }
+                
+                // Noir
+                Button(action: {
+                    self.applyNoir()
+                }) {
+                    CustomButtonText(buttonText: "Noir", buttonColor: Color.green)
+                }
+                
+                // Vintage
+                Button(action: {
+                    self.applyVintage()
+                }) {
+                    CustomButtonText(buttonText: "Vintage", buttonColor: Color.blue)
+                }
+                
+                // Pixellate
+                Button(action: {
+                    self.applyCustom()
+                }) {
+                    CustomButtonText(buttonText: "Pixellate", buttonColor: Color.black)
+                }
+            }
+            .navigationBarTitle("Fiftygram")
+            // Butto to pick the image
+            .navigationBarItems(trailing:
+                Button("Choose Photo") {
+                    self.showingImagePicker = true
+                }
+            )
+        }
+        // a view stacker on top of the current view, pop-up-ish
+        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+            ImagePicker(image: self.$inputImage)
+        }
+    }
+    
+    // load the UIImage data into an image
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
+    }
+    
+    // convert the UIImage picked by the use into a CIImage for filtering
+    func getCGImage() -> CIImage {
+        guard let inputImage = inputImage else {
+            print("inputImage failure")
+            fatalError("inputImage failure")
+        }
+        guard let startImage = CIImage(image: inputImage) else {
+            print("startImage failure")
+            fatalError("inputImage failure")
+        }
+        
+        return startImage
+    }
+    
+    // apply the filter & update the image
+    func applyFilter(filter: CIFilter) {
+        let context = CIContext()
+        
+        // check if filtered image is fine
+        guard let filteredImage = filter.outputImage else {
+            print("outputImage problem")
+            return
+        }
+        
+        // create a CGImage from the filtered image
+        if let cgImage = context.createCGImage(filteredImage, from: filteredImage.extent) {
+            print("cgImage fine")
+            // convert the CGImage to a UIImage
+            let uiImage = UIImage(cgImage: cgImage)
+            // diplay the UIImage
+            image = Image(uiImage: uiImage)
+        }
+        
+        print("cgImage problem")
+    }
+    
+    // apply Sepia filter on image
+    func applySepia() {
+        let startImage = getCGImage()
+
+        let currentFilter = CIFilter.sepiaTone()
+        
+        currentFilter.inputImage = startImage
+        currentFilter.intensity = 1  // sepia intensity
+        
+        applyFilter(filter: currentFilter)
+    }
+    
+    // apply Noir filter on image
+    func applyNoir() {
+        let startImage = getCGImage()
+
+        let currentFilter = CIFilter.photoEffectNoir()
+        
+        currentFilter.inputImage = startImage
+
+        applyFilter(filter: currentFilter)
+    }
+    
+    // apply Vintage filter on image
+    func applyVintage() {
+        let startImage = getCGImage()
+
+        let currentFilter = CIFilter.photoEffectProcess()
+        
+        currentFilter.inputImage = startImage
+        
+        applyFilter(filter: currentFilter)
+    }
+    
+    // apply Custom filter on image
+    func applyCustom() {
+        let startImage = getCGImage()
+
+        let currentFilter = CIFilter.pixellate()
+        
+        currentFilter.scale = 50  // pixellating scale
+        currentFilter.inputImage = startImage
+        
+        applyFilter(filter: currentFilter)
+    }
+}
+
+// Custom View for each button
+struct CustomButtonText: View {
+    
+    @State var buttonText: String
+    @State var buttonColor: Color
+    
+    var body: some View {
+        Text(buttonText)
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding()
+            .font(.title)
+            .foregroundColor(.white)
+            .background(buttonColor)
+            .cornerRadius(36)
     }
 }
 
---
+++ Fiftygram/ImagePicker.swift
@@ -0,0 +1,62 @@
+//
+//  ImagePicker.swift
+//  Fiftygram
+//
+//  Created by Krishna Nanduri on 15/07/20.
+//  Copyright © 2020 Krishna Nanduri. All rights reserved.
+//
+
+import SwiftUI
+
+// builds on View
+// can be used inside SwiftUI directly, doesn't need a body property as the view's body is the view controller itself - it just shows what UIKit returns
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    // to dismiss the view programmatically
+    @Environment(\.presentationMode) var presentationMode
+    
+    // in React terms - lift state up
+    @Binding var image: UIImage?
+    
+    // Coordinators are delegates for UIKit's View Controllers
+    // NSObject - Parent class of almostt everything in UIKit
+    // UIImaePickerControllerDelegate - detect when user selects an image
+    // UINavigationControllerDelegate - detect when user moves between screens
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        // function to actually pick the image
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            // dig through the dictionary to get the selected image
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+            
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+        
+    }
+    
+    // automaticaly called by SwiftUI & creates an instance of the Coordinator class we defined & returns it as the Coordinator of the ImagePicker Struct
+    // this object is automaticaly passed by SwiftUI under context to the below functions
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    // create the initial View (UIImage Picker Controller)
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        // picker object
+        let picker = UIImagePickerController()
+        // assign the coordinator to the picker's delegate
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    // update the view controller if state changes
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+    }
+}
